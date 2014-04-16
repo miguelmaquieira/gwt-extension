e4ff6ba069093b04c5de02054aa4c0dd395d7c80
gael <gael@imotionfactory.com>
2014-04-16 18:26:12 +0200

    Issue #0100: Renderer extended

--- ImotionGWTSTLViewer/src/com/imotion/gwt/stlviewer/client/threejs/EXTGWTTHREE.java
+++ ImotionGWTSTLViewer/src/com/imotion/gwt/stlviewer/client/threejs/EXTGWTTHREE.java
@@ -1,7 +1,6 @@
 package com.imotion.gwt.stlviewer.client.threejs;
 
 import com.akjava.gwt.three.client.THREE;
-import com.imotion.gwt.stlviewer.client.EXTGWTWebGLRenderer;
 
 public class EXTGWTTHREE extends THREE {
 
