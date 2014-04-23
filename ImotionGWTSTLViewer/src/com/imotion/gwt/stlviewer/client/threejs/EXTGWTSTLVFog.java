2ae7c81a5e903a79d28d799c090e18589e028062
gael <gael@imotionfactory.com>
2014-04-23 13:19:12 +0200

    Issue #0100: added more examples

--- ImotionGWTSTLViewer/src/com/imotion/gwt/stlviewer/client/threejs/EXTGWTSTLVFog.java
+++
@@ -1,9 +0,0 @@
-package com.imotion.gwt.stlviewer.client.threejs;
-
-import com.google.gwt.core.client.JavaScriptObject;
-
-public class EXTGWTSTLVFog extends JavaScriptObject {
-
-	protected EXTGWTSTLVFog() {}
-
-}
