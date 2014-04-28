f25866e3346f5d6503416343c8dff47ee9e1a063
gael <gael@imotionfactory.com>
2014-04-28 11:18:06 +0200

    Issue #0200: added exception

--- STLLoaderExample/src/com/example/stl/client/STLLoaderExample.java
+++ STLLoaderExample/src/com/example/stl/client/STLLoaderExample.java
@@ -1,7 +1,10 @@
 package com.example.stl.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.imotion.gwt.stlviewer.client.exception.EXTGWTSTLException;
+import com.imotion.gwt.stlviewer.client.exception.EXTGWTSTLExceptionCallback;
 import com.imotion.gwt.stlviewer.client.widget.threejs.EXTGWTSTLVLoaderWidgetThreeJS;
 
 /**
@@ -17,8 +20,18 @@ public class STLLoaderExample implements EntryPoint {
 		RootPanel rootPanel = RootPanel.get();
 		rootPanel.setWidth("640px");
 		
+		//Error capture
+		EXTGWTSTLExceptionCallback exceptionCallback = new EXTGWTSTLExceptionCallback() {
+			
+			@Override
+			public void onFailure(EXTGWTSTLException exception) {
+				Window.alert(exception.getMessage());
+			}
+		};
+		
 		EXTGWTSTLVLoaderWidgetThreeJS rendererWidget = new EXTGWTSTLVLoaderWidgetThreeJS(
 																"https://dl.dropboxusercontent.com/u/62612071/imotion/stl/COLONEL_72k_v2.stl",
+																exceptionCallback,
 																0xA09595, 0xFFFFFF, 640, 480);
 		rootPanel.add(rendererWidget);
 		rendererWidget.setWidth("100%");
