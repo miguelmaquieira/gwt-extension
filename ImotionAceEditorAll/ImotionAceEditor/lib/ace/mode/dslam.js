92c63927f7cd2338e1bb05d3ac766816ba84bbdc
gael <gael@imotionfactory.com>
2014-06-02 18:06:23 +0200

    Issue #0300: working on language inheritance

--- ImotionAceEditorAll/ImotionAceEditor/lib/ace/mode/dslam.js
+++ ImotionAceEditorAll/ImotionAceEditor/lib/ace/mode/dslam.js
@@ -5,12 +5,16 @@ var oop = require("../lib/oop");
 var TextMode = require("./text").Mode;
 var DslamHighlightRules = require("./dslam_highlight_rules").DslamHighlightRules;
 var FoldMode = require("./folding/cstyle").FoldMode;
+var ImoLangMode = require("./imolang").Mode;
 
 var Mode = function() {
     this.HighlightRules = DslamHighlightRules;
     this.foldingRules = new FoldMode();
+    this.createModeDelegates({
+        "imo-": ImoLangMode
+    });
 };
-oop.inherits(Mode, TextMode);
+oop.inherits(Mode, ImoLangMode);
 
 (function() {
     this.lineCommentStart = "//";
