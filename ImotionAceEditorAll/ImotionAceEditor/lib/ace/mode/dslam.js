99736674196ee14990c249564a98d478702a0a90
gael <gael@imotionfactory.com>
2014-06-03 13:56:20 +0200

    Issue #0300: dslam without inheritance

--- ImotionAceEditorAll/ImotionAceEditor/lib/ace/mode/dslam.js
+++ ImotionAceEditorAll/ImotionAceEditor/lib/ace/mode/dslam.js
@@ -5,16 +5,12 @@ var oop = require("../lib/oop");
 var TextMode = require("./text").Mode;
 var DslamHighlightRules = require("./dslam_highlight_rules").DslamHighlightRules;
 var FoldMode = require("./folding/cstyle").FoldMode;
-var ImoLangMode = require("./imolang").Mode;
 
 var Mode = function() {
     this.HighlightRules = DslamHighlightRules;
     this.foldingRules = new FoldMode();
-    this.createModeDelegates({
-        "imo-": ImoLangMode
-    });
 };
-oop.inherits(Mode, ImoLangMode);
+oop.inherits(Mode, TextMode);
 
 (function() {
     this.lineCommentStart = "//";
