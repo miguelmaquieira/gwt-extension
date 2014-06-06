fdaafe72802b883a6de79cca9bea548101690496
gael <gael@imotionfactory.com>
2014-06-06 10:34:53 +0200

    Issue #0300: added filePersistence to persistenceServiceBase

--- DSLAMAll/DSLAM-ANTLR/src/main/antlr4/DSLAM.g4
+++ DSLAMAll/DSLAM-ANTLR/src/main/antlr4/DSLAM.g4
@@ -6,27 +6,48 @@ grammar DSLAM;
 
 
 //PARSER
-program :	block+;
-block   :	assign
-			| exec;
-assign: VARIABLE_SCRIPT '=' expression  ';';
+program :	statement+;
+statement   :	assignStatement
+			|   ifStatement
+			|	whileStatement
+			|	forStatement ;
+//			| exec;
+assignStatement: VARIABLE_SCRIPT '=' expression  ';';
+
+ifStatement: 	'if' condition '{' statement+ '}' ('else' '{' statement+ '}')?;
+whileStatement: 'while' condition '{' statement+ '}';
+forStatement: 	'for' VARIABLE_SCRIPT 'in' '(' (INTEGER|VARIABLE_SCRIPT|VARIABLE_PROCESS|VARIABLE_EXTERNAL) '..' (INTEGER|VARIABLE_SCRIPT|VARIABLE_PROCESS|VARIABLE_EXTERNAL) ')' '{' statement+ '}';
+
 expression : INTEGER							  #atom
 			 | VARIABLE_SCRIPT					  #atom
 			 | expression ('*' | '/') expression  #aritOp
 			 | expression ('+' | '-') expression  #aritOp
 			 ;
-exec : 'execute' (StringCharacters (VARIABLE_SCRIPT)*)+ ';';
+//exec : 'execute' COMMAND_ITEM+ ';';
+condition : '(' (INTEGER|variable) (LOGICAL_COMPARATOR (INTEGER|variable))? ')';
+
+variable : 	VARIABLE_SCRIPT
+			| VARIABLE_PROCESS
+			|VARIABLE_EXTERNAL;
 
 //LEXER
 INTEGER: (DIGIT)+;
 WS  :  [ \t\r\n\u000C]+ -> skip
     ;
+VARIABLE_SCRIPT : 	'$' VARIABLE_NAME;
+VARIABLE_PROCESS : 	'%' VARIABLE_NAME;
+VARIABLE_EXTERNAL : '#' VARIABLE_NAME;
 VARIABLE_NAME : IDENT_CHAR ('_'| IDENT_CHAR)+ ;
-VARIABLE_SCRIPT : '$' VARIABLE_NAME;
+COMMAND_ITEM	: (IDENT_CHAR)(IDENT_CHAR | ':' | '_' |'-' | '/')+;
+LOGICAL_COMPARATOR :   '=='
+					 | '!='
+					 | '<'
+					 | '<='
+					 | '>'
+					 | '>=';
+STRING_LITERAL:   '"' STRING_CHARACTERS? '"';
 
 fragment	DIGIT 				: '0'..'9' ;
 fragment	IDENT_CHAR 			: ('a'..'z'|'A'..'Z');
-fragment	StringCharacter		:   [\u0000-\uFFFF];
-fragment	StringCharacters	:   StringCharacter+;
-
-StringLiteral:   '"' StringCharacters? '"';
+fragment	STRING_CHARACTERS	:   STRING_CHARACTER+;
+fragment	STRING_CHARACTER	:   ~["\\];
--- DSLAMAll/DSLAMBackend/src/main/java/com/imotion/dslam/backend/persistence/jpa/DSLAMBKPersistenceServiceBaseJPA.java
+++ DSLAMAll/DSLAMBackend/src/main/java/com/imotion/dslam/backend/persistence/jpa/DSLAMBKPersistenceServiceBaseJPA.java
@@ -2,14 +2,19 @@ package com.imotion.dslam.backend.persistence.jpa;
 
 import java.io.Serializable;
 
-import com.selene.arch.exe.back.persistence.AEMFTPersistenceServiceBase;
+import com.imotion.dslam.backend.persistence.DSLAMBKPersistenceServiceBase;
+import com.imotion.dslam.backend.persistence.service.file.DSLAMBKFilePersistenceServiceJPA;
+import com.selene.arch.exe.back.persistence.AEMFTIPersistenceService;
 import com.selene.arch.exe.back.persistence.module.AEMFTIPersistenceModule;
 
-public abstract class DSLAMBKPersistenceServiceBaseJPA<T, Q extends T, Id extends Serializable> extends AEMFTPersistenceServiceBase<T, Q, Id> {
+public abstract class DSLAMBKPersistenceServiceBaseJPA<T, Q extends T, Id extends Serializable> extends DSLAMBKPersistenceServiceBase<T, Q, Id> {
 
 	private static final long serialVersionUID = -3861653720997741685L;
 
 	private AEMFTIPersistenceModule<Q, Id> persistenceModule;
+	
+	private DSLAMBKFilePersistenceServiceJPA filePersistence;
+	
 	
 	@Override
 	public AEMFTIPersistenceModule<Q, Id> getPersistenceModule() {
@@ -18,6 +23,31 @@ public abstract class DSLAMBKPersistenceServiceBaseJPA<T, Q extends T, Id extend
 			persistenceModule.initialize(new Object[] { getPersistenceCoreService(), getPersistenceClass()});
 		}
 		return persistenceModule;
+	}
+	
+	/**************************************************************
+     *                AEMFTIFACTORABLE FUNCTIONS                  *
+     **************************************************************/
+
+	@Override
+	public void releaseInstance() {
+		super.releaseInstance();
+		persistenceModule 		= null;
+		if (filePersistence != null) {
+			getFactoryPersistence().release((AEMFTIPersistenceService<?, ?, ?>) filePersistence);
+			filePersistence = null;
+		}
+	}
+	
+	/**************************************************************
+     *                   PROTECTED FUNCTIONS                      *
+     **************************************************************/
+	
+	protected DSLAMBKFilePersistenceServiceJPA getFilePersistence() {
+		if (filePersistence == null) {
+			filePersistence = (DSLAMBKFilePersistenceServiceJPA) getFactoryPersistence().newFilePersistence();
+		}
+		return filePersistence;
 	}
 
 }
